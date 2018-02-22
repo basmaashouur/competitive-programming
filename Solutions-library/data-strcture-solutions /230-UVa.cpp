474afb888ae99f65a55623fa2239f2a30e79931e
Basma Ashour <basmaashouur@gmail.com>
2018-02-22 21:10:20 +0200

    Create 230-UVa.cpp

---
+++ Solutions-library/data-strcture-solutions /230-UVa.cpp
@@ -0,0 +1,11 @@
+/*
+Name: 230 - Borrowers
+Online Judge: UVa
+Tags: vectors, sorting
+Tutorial: put all the title in vector<pair<string, int>> sort them all int would be 1, 
+when "borrow" then make title index = -1, when "return" put all the return in vector<string> 
+when "Shelve" change the -1 to 1 in the vector of that value and see what is the 1 that is before then print it
+*/
+
+
+// C O D E
