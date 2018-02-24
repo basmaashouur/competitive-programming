55efde643e01a3b5369c47e0dd3ac632fd794b55
Basma Ashour <basmaashouur@gmail.com>
2018-02-24 08:50:39 +0200

    Update 230-UVa.cpp

--- Solutions-library/data-strcture-solutions /230-UVa.cpp
+++ Solutions-library/data-strcture-solutions /230-UVa.cpp
@@ -1,11 +1,104 @@
 /*
 Name: 230 - Borrowers
 Online Judge: UVa
-Tags: vectors, sorting
-Tutorial: put all the title in vector<pair<string, int>> sort them all int would be 1, 
-when "borrow" then make title index = -1, when "return" put all the return in vector<string> 
-when "Shelve" change the -1 to 1 in the vector of that value and see what is the 1 that is before then print it
+Tags: vectors, sorting, strings
+Tutorial: make a string parsing with the input then put it in vector<pair<author,title >> sort them all 
+now we don't need author name anymore replace it with "1" it means the book is on the shelve
+when "borrow" then make first pair= "-1", when "return" put all the return books in a vector
+when "Shelve" change the "-1" to "1" in the vector of that value and put them all in their original 
+index but in a new vector and then see what is the "1" that is before it then print them.
 */
 
 
 // C O D E
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+
+  vector < pair < string, string >> vec;
+  vector < string > ret;
+  while (true) {
+    string val, title, auth;
+    int index;
+    getline(cin, val);
+    if (val == "END") break;
+
+    // save the title
+    title += '"';
+    for (int x = 1; x < val.length(); x++) 
+    {
+      title += val[x];
+      if (val[x] == '"') 
+      {
+        index = x + 1;
+        break;
+      }
+    }
+    // save the author
+    auth.append(val.begin() + index, val.end());
+
+    vec.push_back(make_pair(auth, title));
+  }
+
+  sort(vec.begin(), vec.end());
+  for (int x = 0; x < vec.size(); x++)
+    vec[x].first = "1";
+    
+  while (true) 
+  {
+    string val, fin;
+    getline(cin, val);
+    if (val == "END") break;
+
+    //shelve
+    if (val[5] == 'E') {
+      vector < string > shelve(vec.size());
+      for (int x = 0; x < ret.size(); x++) 
+      {
+        string rett = ret[x];
+        for (int y = 0; y < vec.size(); y++)
+         {
+          if (rett == vec[y].second) 
+          {
+            shelve[y] = rett, vec[y].first = "1";
+            break;
+          }
+        }
+      }
+      // print the shelve
+      string before = "";
+      for (int x = 0; x < shelve.size(); x++) 
+      {
+        if (shelve[x] != "" && before == "") cout << "Put " << shelve[x] << " first" << endl;
+        else if (shelve[x] != "") cout << "Put " << shelve[x] << " after " << before << endl;
+        if (vec[x].first == "1") before = vec[x].second;
+      }
+      cout << "END" << endl;
+      ret.clear();
+    }
+
+    //borrow
+    else if (val[5] == 'W') 
+    {
+      fin.append(val.begin() + 7, val.end());
+      for (int x = 0; x < vec.size(); x++)
+        if (vec[x].second == fin) 
+        {
+          vec[x].first = "-1";
+          break;
+        }
+    }
+    // return
+    else 
+    {
+      fin.append(val.begin() + 7, val.end());
+      ret.push_back(fin);
+    }
+  }
+
+  return 0;
+}
