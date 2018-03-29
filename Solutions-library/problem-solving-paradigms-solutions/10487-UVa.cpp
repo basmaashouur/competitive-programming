a08c64bda6aabc50a08ece791ab598b4698d42b8
Basma Ashour <basmaashouur@gmail.com>
2018-03-29 23:07:15 +0200

    Create 10487-UVa.cpp

---
+++ Solutions-library/problem-solving-paradigms-solutions/10487-UVa.cpp
@@ -0,0 +1,12 @@
+/*
+Name: 10487 - Closest Sums
+Online Judge: UVa
+Tags: Iterative
+Complexity: O(n^2)
+Tutorial: Sort the numbers, then for every query itrate over the numbers and sum every two consecutive numbers,
+and see what is closet to the query, that would take n itartion AKA linear search,
+we can use binary search and it would take log n.
+*/
+
+
+// C O D E
