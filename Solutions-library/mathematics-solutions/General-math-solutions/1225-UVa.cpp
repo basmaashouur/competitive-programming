69d671210d569fcb3ac3d4dc0b244098a8a63f2e
Basma Ashour <basmaashouur@gmail.com>
2018-06-10 13:48:32 +0200

    Create 1225-UVa.cpp

---
+++ Solutions-library/mathematics-solutions/General-math-solutions/1225-UVa.cpp
@@ -0,0 +1,40 @@
+/*
+Name: 1225 - Digit Counting
+Online Judge: UVa
+Tags: Brute Force math
+Complexity:
+Tutorial: Find the sum of all the divsiors of a number then check it's type.
+*/
+
+
+// C O D E
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	cout << "PERFECTION OUTPUT" << endl;
+	while (true)
+	{
+		int n, ans = 0;
+		cin >> n;
+		if (n == 0)
+		{
+			cout << "END OF OUTPUT" << endl;
+			break;
+		}
+		for (int x = 1; x <= n/2 ; x++)
+		{
+			if (n % x == 0)ans += x;
+		}
+		printf("%5d  ", n);
+		if (ans == n) cout << "PERFECT" << endl;
+		if (ans > n)cout << "ABUNDANT" << endl;
+		if (ans < n)cout << "DEFICIENT" << endl;
+
+	}
+	return 0;
+}
